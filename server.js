70673c28a7fc100a8f78840aecda9680a58b2d38
Brendon Hart <brendonhartbusiness@gmail.com>
2020-11-20 16:55:50 -0700

    added a T

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 const opts = {
   useNewUrlParser: true,
   useUnifiedTopology: false,
-  useCreaeIndex: true,
+  useCreateIndex: true,
   useFindAndModify: false
 };
 
